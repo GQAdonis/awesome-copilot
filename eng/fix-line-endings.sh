9199dbc4f7043068d42c272e2771a88a6339d63f
Travis James <travis@tribehealthsolutions.com>
2026-09-24 05:14:36 -0500

    fix(eng): make fix-line-endings.sh portable to BSD sed on macOS

--- eng/fix-line-endings.sh
+++ eng/fix-line-endings.sh
@@ -4,6 +4,13 @@
 echo "Normalizing line endings in markdown files..."
 
 # Find all markdown files and convert CRLF to LF
-find . -name "*.md" -type f -exec sed -i 's/\r$//' {} \;
+# Use GNU sed in-place syntax on Linux, BSD sed syntax on macOS.
+if sed --version >/dev/null 2>&1; then
+  SED_IN_PLACE=(-i)
+else
+  SED_IN_PLACE=(-i '')
+fi
+
+find . -name "*.md" -type f -exec sed "${SED_IN_PLACE[@]}" 's/\r$//' {} \;
 
 echo "Done! All markdown files now have LF line endings."
